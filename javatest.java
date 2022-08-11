91c0761095c2c7756ccaf2bd141954ab08c0a03e
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-08-11 16:05:37 +0200

    Working GUI

--- javatest.java
+++ javatest.java
@@ -4,19 +4,11 @@ import javax.swing.BorderFactory;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
-class test{
-    public test(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,100,300));
-        panel.setLayout(new GridLayout(0, 1));
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("GUI");
-        frame.pack();
-    }
+class test extends JFrame{
     public static void main(String [] args){
-        new test();
+      test win = new test();
+      win.setSize(500,500);
+      win.setVisible(true);
+      win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
