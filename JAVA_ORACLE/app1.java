1eb4349e9168aa182dbcbab5a9b7428a7d2bb74b
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-10-16 22:15:16 +0200

    JAVA CERTIFICATE

---
+++ JAVA_ORACLE/app1.java
@@ -0,0 +1,10 @@
+public class app1{
+    public static void main(String[] args){
+        /**
+         * Main in my prog
+         */
+        System.out.println("Hello World");
+        float d = 1.2f;
+        System.out.println(d);
+    }
+}
