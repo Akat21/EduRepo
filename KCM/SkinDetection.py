88d5261c496ff3fa67953340939355224f45b84d
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-10-16 13:54:28 +0200

    Finished lab1

--- KCM/SkinDetection.py
+++ KCM/SkinDetection.py
@@ -1,20 +1,20 @@
-from colorsys import rgb_to_hsv
 import matplotlib.pyplot as plt
 import matplotlib.image as image
-import matplotlib.colors as colors
 import numpy as np
 import cv2
 
-def NormalizeData(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
+lower = np.array([0*255, 0.2*255, 0.4*255], dtype = "float")
+upper = np.array([0.1*255, 0.6*255, 1*255], dtype = "float")
 
-lower = np.array([0, 48, 80], dtype = "uint8")
-upper = np.array([20, 255, 255], dtype = "uint8")
-
+img0 = plt.imread("hand.jpg")
 img = cv2.imread("hand.jpg")
 img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 skinMask = cv2.inRange(img_hsv, lower, upper)
+
 skin = cv2.bitwise_and(img, img, mask = skinMask)
 skin[skin != 0] = 255
-plt.imshow(skin)
+
+f, axarr = plt.subplots(1,2)
+axarr[0].imshow(img0)
+axarr[1].imshow(skin)
 plt.show() 
