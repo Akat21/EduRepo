5b1a7072b0b899de5dea7358af0dcb51c97d7ff7
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-10-28 16:36:45 +0200

    Code cleaning

--- SI/DFS.py
+++ SI/DFS.py
@@ -1,60 +1,46 @@
 board = []
 result = []
 ###inteligentne - sprawdzanie bic zawsze
-def board_gen(n, board, result):
-    '''Board Generation using DFS'''
-    #counter = 0
+
+def DFS(n, board, result):
+    '''Possible boards using DFS'''
+    counter = 0
     if len(board) == 0:
         for i in range(n):
             board.append([str(i)])
-            #counter += 1
+            counter += 1
     while len(board[-1][0]) < n:
         last_el = board[-1] 
         del board[-1]
         for i in range(n):
-            #counter += 1
+            counter += 1
             board.append([''.join(last_el) + str(i)])
             if (len(board[-1][0]) == n):
-                for idx, el in enumerate(board[-1][0]):
-                    if len(board) == 0:
-                        break
-                    for idx1, el1 in enumerate(board[-1][0]):
-                        if idx1 > idx:
-                            if attack(el, idx, el1, idx1) == True:
-                                del board[-1]
-                                break
-                            else:
-                                continue
-                        else:
-                            continue
+                board = check_captures(board)
         if len(board) == 0:
                     break
     if len(board) != 0:
         result.append(board[-1][0])
         del board[-1]
-        return board_gen(n, board, result)
+        return DFS(n, board, result)
     else:
-        return result
+        return result, counter
         
-    #print("Liczba stanów wygenerowanych: ", counter)
-    #return board, counter
-
-def capturing(board):
-    '''Captures in every position'''
-    #cnt = 0
-    result = []
-    for pos in board:
-        score = 0
-        #cnt += 1
-        for idx, el in enumerate(pos[0]):
-            for idx1, el1 in enumerate(pos[0]):
-                if idx1 > idx:
-                    if attack(el, idx, el1, idx1) == True:
-                        score += 1
-        result.append([score, pos])
-    #print("Liczba Stanów sprawdzonych: ", cnt)
-    return result
-
+def check_captures(board):
+    '''Check if position is valid then delete the wrong one'''
+    for idx, el in enumerate(board[-1][0]):
+        if len(board) == 0:
+            break
+        for idx1, el1 in enumerate(board[-1][0]):
+            if idx1 > idx:
+                if attack(el, idx, el1, idx1) == True:
+                    del board[-1]
+                    break
+                else:
+                    continue
+            else:
+                continue
+    return board
 
 def attack(qrow, qcolumn, orow, ocolumn):
     '''Check if there is possible capture'''
@@ -67,4 +53,5 @@ def attack(qrow, qcolumn, orow, ocolumn):
     else:
         return False
 
-print(board_gen(4,board,result))
+board_1, counter = DFS(4,board,result)
+print(board_1, "\nLiczba stanów wygenerowanych:", counter)
