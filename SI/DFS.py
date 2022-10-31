c821a487c767e7d35998372cae88696f596c66bc
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-10-31 17:53:30 +0100

    Intelligent BFS DFS added

--- SI/DFS.py
+++ SI/DFS.py
@@ -44,6 +44,66 @@ def BFS(n, board, result):
             board, result = BFS_captures(board)
     return result, counter
 
+def Intel_DFS(n, board, result):
+    '''Possible boards using DFS'''
+    counter = 0
+    if len(board) == 0:
+        for i in range(n):
+            board.append([str(i)])
+            counter += 1
+    while len(board[-1][0]) < n:
+        last_el = board[-1] 
+        del board[-1]
+        for i in range(n):
+            counter += 1
+            board.append([''.join(last_el) + str(i)])
+            board = DFS_captures(board)
+        if len(board) == 0:
+            break
+    if len(board) != 0:
+        result.append(board[-1][0])
+        del board[-1]
+        return Intel_DFS(n, board, result)
+    else:
+        return result, counter
+
+def Intel_BFS(n, board, result):
+    '''Possible boards using BFS'''
+    counter = 0 #TODO
+    if len(board) == 0:
+        for i in range(n):
+            board.append([str(i)])
+            counter += 1
+    while len(board[0][0]) < n:
+        for i in range(n):
+            counter += 1
+            board.append([''.join(board[0]) + str(i)])
+            board, pos = BFS_captures1(board)
+            if len(pos[0]) == n:
+                result.append(pos)
+        del board[0]
+        if len(board) == 0:
+            break
+    return result, counter
+
+def BFS_captures1(board):
+    '''Check if position is valid then delete the wrong one'''
+    result = []
+    for idx, el in enumerate(board[-1][0]):
+        if len(board) == 0:
+            break
+        for idx1, el1 in enumerate(board[-1][0]):
+            if idx1 > idx:
+                if attack(el, idx, el1, idx1) == True:
+                    del board[-1]
+                    return board, "0"
+                else:
+                    continue
+            else:
+                continue
+    result.append(board[-1][0])
+    return board, result
+
 def DFS_captures(board):
     '''Check if position is valid then delete the wrong one'''
     for idx, el in enumerate(board[-1][0]):
@@ -122,5 +182,12 @@ def BFS_DFS_Comparision(x, y):
         end1 = time.time()
         print("Czas wykonania:", end1 - start1)
 
-DFS_Stats(4,11)
+board = []
+result = []
+
+print(Intel_BFS(4,board,result))
+board = []
+result = []
+print(Intel_DFS(4,board,result))
+#DFS_Stats(4,11)
 #BFS_DFS_Comparision(4,8)
