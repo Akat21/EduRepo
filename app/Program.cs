680d3578ea2f3ce52719bfd99ae0dab518c341a3
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-11-05 14:26:49 +0100

    CS beginnings

---
+++ app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 17;
+            Console.WriteLine("Hello: {0}", Convert.ToChar(num));
+        }
+    }
+}
