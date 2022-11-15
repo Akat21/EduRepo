18157198149dc0e304455da4cd29e6921fc73f68
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-11-15 21:27:25 +0100

    working q

--- IPZ/testing.py
+++ IPZ/testing.py
@@ -1,27 +1,37 @@
 import cv2
 import mediapipe as mp
 import time
-import HandTrackingModule as htm
-import PoseEstimationModule as pem
+import Modules.HandTrackingModule as htm
 
 def main():
     pTime = 0
 
     cap = cv2.VideoCapture(0)
-    detector = pem.poseDetector()
+    detector = htm.handDetector(maxHands=1)
 
     while True:
         success, img = cap.read()
-        img = detector.findPose(img)
+        img = detector.findHands(img)
+        overlay = img.copy()
         lmList = detector.findPosition(img)
         if len(lmList) != 0:
-            print(lmList[4])
+            print(lmList[8])
 
         cTime = time.time()
         fps = 1/(cTime - pTime)
         pTime = cTime
-    
-        cv2.putText(img, str(int(fps)),(10,30), cv2.FONT_HERSHEY_PLAIN, 2 ,(255,0,255), 3)
+
+        x, y, w, h = 90, 100, 50, 50
+        cv2.rectangle(img, (90, 100), (x+w, y+h), (0,0,0), -1)
+        alpha = 0.7
+        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+        cv2.putText(img, 'q', (100,140), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
+        try:
+            if lmList[8][1] > 90 and lmList[8][1] < 140 and lmList[8][2] > 100 and lmList[8][2] < 150:
+                cv2.putText(img, 'q', (300,140), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
+        except:
+            pass
+        cv2.putText(img, str(int(fps)),(0,15), cv2.FONT_HERSHEY_PLAIN, 1 ,(255,0,255), 2)
         cv2.imshow("Image", img)
         cv2.waitKey(1)
 
