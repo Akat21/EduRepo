ab2f4bfba15744d41db3a62a5b1b38e94d83e979
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-11-16 13:41:56 +0100

    Testing class

--- IPZ/testing.py
+++ IPZ/testing.py
@@ -11,29 +11,37 @@ def main():
 
     while True:
         success, img = cap.read()
+        img = cv2.flip(img, 1)
         img = detector.findHands(img)
         overlay = img.copy()
         lmList = detector.findPosition(img)
         if len(lmList) != 0:
             print(lmList[8])
 
+        w, h = 50, 50
+        cv2.rectangle(img, (90, 100), (90 + w, 90 + h), (0,0,0), -1)
+        cv2.rectangle(img, (150, 100), (150 + w, 90 + h), (0,0,0), -1)
+        alpha = 0.7
+        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+        cv2.putText(img, 'Q', (100,135), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
+        cv2.putText(img, 'W', (160,135), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
+        try:
+            if lmList[8][1] > 90 and lmList[8][1] < 140 and lmList[8][2] > 100 and lmList[8][2] < 150:
+                cv2.putText(img, 'Q', (300,135), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
+            if lmList[8][1] > 150 and lmList[8][1] < 200 and lmList[8][2] > 100 and lmList[8][2] < 150:
+                cv2.putText(img, 'W', (300,135), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
+        except:
+            pass
+        
+        ###FPS###
         cTime = time.time()
         fps = 1/(cTime - pTime)
         pTime = cTime
 
-        x, y, w, h = 90, 100, 50, 50
-        cv2.rectangle(img, (90, 100), (x+w, y+h), (0,0,0), -1)
-        alpha = 0.7
-        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
-        cv2.putText(img, 'q', (100,140), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
-        try:
-            if lmList[8][1] > 90 and lmList[8][1] < 140 and lmList[8][2] > 100 and lmList[8][2] < 150:
-                cv2.putText(img, 'q', (300,140), cv2.FONT_HERSHEY_PLAIN, 3 ,(255,255,255), 2)
-        except:
-            pass
         cv2.putText(img, str(int(fps)),(0,15), cv2.FONT_HERSHEY_PLAIN, 1 ,(255,0,255), 2)
         cv2.imshow("Image", img)
         cv2.waitKey(1)
+        #########
 
 if __name__ == '__main__':
     main()
