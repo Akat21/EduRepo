7490bee237a37daf01b8f046705c395310d42ffb
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-11-25 15:03:41 +0100

    EKG Done

--- KCM/EKG_read.py
+++ KCM/EKG_read.py
@@ -9,26 +9,42 @@ def peaks(data):
     peak_idx , peak_val = find_peaks(data.to_numpy().flatten(), height = 0)
     return peak_idx, peak_val['peak_heights']
 
+def filter(peak_idx, peak_val):
+    peak_val1, peak_idx1 = [], []
+    for idx, el in enumerate(peak_idx):
+        if idx == 0:
+            peak_idx1.append(el)
+            peak_val1.append(peak_val[idx])
+            continue
+        else:
+            if(el - (peak_idx[idx-1]) < 135):
+                continue
+            else:
+                peak_idx1.append(el)
+                peak_val1.append(peak_val[idx])
+    return peak_idx1, peak_val1
+
 def read_EKG(file, fs = 360):
     A = pd.read_csv(file)
     One_sec = A[0:fs]
     Two_sec = A[0:fs*2]
-    peak_idx , peak_val = find_peaks(Two_sec.to_numpy().flatten(), height = 0)
+    peak_idx, peak_val = peaks(Two_sec)
+    peak_idx, peak_val = filter(peak_idx, peak_val)
+
     cycle = A[peak_idx[0]:peak_idx[1]]
     HR = fs/(np.abs(peak_idx[1]-peak_idx[0]))*60
     print(HR)
 
     HR = []
     peak_idx, peak_val = peaks(A)
+    peak_idx, peak_val = filter(peak_idx, peak_val)
+
     for idx, el in enumerate(peak_idx):
-        if idx == 0:
-            continue
-        else:
-            if(el - peak_idx[idx-1] > 135):
-                HR.append(fs/(np.abs(el-peak_idx[idx-1]))*60)
+        HR.append(fs/(np.abs(el-peak_idx[idx-1]))*60)
 
-    print("\n", np.mean(HR))
-
+    print("\n Średnia", np.mean(HR))
+    print("\n STD", np.std(HR))
+    print("\n VAR", np.var(HR))
 
     plt.plot(peak_idx, peak_val, "ro")
     plt.plot(A)
