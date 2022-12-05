3d392ce061a1832b05835116910e63c501786b76
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-12-05 20:42:03 +0100

    Working NBC

--- SI/NBC/NBC.py
+++ SI/NBC/NBC.py
@@ -8,39 +8,45 @@ class NBC(BaseEstimator, ClassifierMixin):
     def __init__(self):
         pass
 
-    def fit(self, X, y):
+    def fit(self, X, y, LaPlace = False):
         '''First element = type, Second element = occurances, Third elemnt = probability'''
-        self.P_Y = self.Single_Prob(y)
-        self.P_X_Y = self.Condi_Prob(X, y)
+        self.P_Y = self.Single_Prob(y, LaPlace)
+        self.P_X_Y = self.Condi_Prob(X, y, LaPlace)
+
+    def accuracy_score(self, X_predict, y_test):
+        res_list = X_predict == y_test
+        return np.round((len(res_list[res_list==True])/len(res_list)) * 100)
 
     def predict(self, P_X):
         res = []
         cnt = 0
+        temp = np.array(self.P_Y)[:,0]
         for el in P_X:
             P = []
-            for classi_num in range(1,len(self.P_Y)+1):
+            for idx, classi_num in enumerate(temp):
                 prob = 1
                 for col in self.P_X_Y:
                     for classi in col:
                         if classi[0][0] == classi_num:
                             for est in classi:
-                                if est[1] == el[cnt]:
-                                    prob *= est[2]
+                                if str(est[1]) == str(el[cnt]):
+                                    prob *= est[2].astype(np.float64)
                                     cnt += 1 
                                     break
                             break
                 cnt = 0       
-                prob *= self.P_Y[classi_num - 1][2]
+                prob *= self.P_Y[idx][2]
                 P.append(prob)
-            res.append(P.index(max(P)) + 1)
+            res.append(self.P_Y[P.index(max(P))][0])
         return res
     
     def predict_proba(self, P_X):
         res = []
         cnt = 0
+        temp = np.array(self.P_Y)[:,0]
         for el in P_X:
             P = []
-            for classi_num in range(1,len(self.P_Y)+1):
+            for idx, classi_num in enumerate(temp):
                 prob = 1
                 for col in self.P_X_Y:
                     for classi in col:
@@ -52,37 +58,46 @@ class NBC(BaseEstimator, ClassifierMixin):
                                     break
                             break
                 cnt = 0       
-                prob *= self.P_Y[classi_num - 1][2]
+                prob *= self.P_Y[idx][2]
                 P.append(prob)
             res.append(P)
-        res1 = res[2][2] / (res[2][0] + res[2][1] + res[2][1]) ##dla wszystkich wynikow res obliczyc
-        return res1
 
-    def Single_Prob(self, y):
+        final_res = []
+        for i in range(len(res)):
+            temp_res = []
+            for j in range(len(res[i])):
+                temp_res.append(res[i][j] / (sum(res[i])))
+            final_res.append(temp_res)
+        return final_res
+
+    def Single_Prob(self, y, LaPlace):
         P_cnt = collections.Counter(y)
         x = list(dict(P_cnt).keys())
         P = []
         for el in x:
-            P.append([int(el), dict(P_cnt)[el], dict(P_cnt)[el]/sum(P_cnt.values())])
+            if LaPlace == True:
+                P.append([el, dict(P_cnt)[el], (dict(P_cnt)[el] + 1)/(sum(P_cnt.values()) + len(x))])
+            elif LaPlace == False:
+                P.append([el, dict(P_cnt)[el], dict(P_cnt)[el]/sum(P_cnt.values())]) 
         return P
 
-    def Condi_Prob(self, X, y):
+    def Condi_Prob(self, X, y, LaPlace):
         '''Returns probability (first dimension is first column etc)'''
         P_semifinal = []
         P_final = []
-        P_Y = self.Single_Prob(y)
+        P_Y = self.Single_Prob(y, LaPlace)
 
         for col_num in range(len(X[0])):
             P_semifinal = []
             for y_el_num in range(len(P_Y)):
                 P = []
                 P_X = []
-
+                
                 ##Liczymy prawdopodobienstwo wystąpienia estymowanej wartosci X dla kolumny w kazdym estymatorze y(są 3 - 1,2,3)
                 for idx, est in enumerate(y):
                     if est == P_Y[y_el_num][0]:
                         P_X.append(X[idx, col_num])
-                P_X = self.Single_Prob(P_X)
+                P_X = self.Single_Prob(P_X, LaPlace)
                 
                 ##Jeżeli nie ma wszystkich wartości w kolumnie dodajemy kolumne o prawdopodobienstwie 0.0 dla brakujacego elementu
                 ##nwm czy potrzebne
--- SI/NBC/testing.py
+++ SI/NBC/testing.py
@@ -3,19 +3,60 @@ from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, ClassifierMixin
 from NBC import NBC 
+import pandas as pd
 
-X = np.genfromtxt(fname = 'wine.data', delimiter = ',', )
+X = np.array(pd.read_csv("wine.data"))
 y = X[:,0]
 X = X[:,1:]
 
 est = KBinsDiscretizer(n_bins = 3, encode='ordinal', strategy='uniform').fit(X)
 X = est.transform(X)
+with_laplace = []
+without_laplace = []
 
-X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.33, random_state=42)
+for i in range(200):
 
-nbc = NBC()
-nbc.fit(X_train,y_train)
-prediction = nbc.predict(X_test)
-prediction_prob = nbc.predict_proba(X_test)
-# print(P_Y,"\n", P_X_Y)
-print(prediction_prob)
+    X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.33)
+    nbc = NBC()
+    nbc.fit(X_train,y_train, LaPlace=False)
+    X_predict = nbc.predict(X_test)
+    prediction_prob = nbc.predict_proba(X_test)
+    with_laplace.append(nbc.accuracy_score(X_predict, y_test))
+
+    nbc = NBC()
+    nbc.fit(X_train,y_train, LaPlace=True)
+    X_predict = nbc.predict(X_test)
+    prediction_prob = nbc.predict_proba(X_test)
+    nbc.accuracy_score(X_predict, y_test)
+    without_laplace.append(nbc.accuracy_score(X_predict, y_test))
+    
+print("Bez poprawki LaPlace'a: ", np.mean(without_laplace))
+print("Z poprawką LaPlace'a: ", np.mean(with_laplace))
+
+X = np.array(pd.read_csv("balance-scale.data"))
+y = X[:,0]
+X = X[:,1:]
+est = KBinsDiscretizer(n_bins = 3, encode='ordinal', strategy='uniform').fit(X)
+X = est.transform(X)
+with_laplace = []
+without_laplace = []
+
+for i in range(100):
+
+    X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.33)
+    nbc = NBC()
+    nbc.fit(X_train,y_train, LaPlace=False)
+    X_predict = nbc.predict(X_test)
+    prediction_prob = nbc.predict_proba(X_test)
+    with_laplace.append(nbc.accuracy_score(X_predict, y_test))
+
+    nbc = NBC()
+    nbc.fit(X_train,y_train, LaPlace=True)
+    X_predict = nbc.predict(X_test)
+    prediction_prob = nbc.predict_proba(X_test)
+    nbc.accuracy_score(X_predict, y_test)
+    without_laplace.append(nbc.accuracy_score(X_predict, y_test))
+    
+print("Bez poprawki LaPlace'a: ", np.mean(without_laplace))
+print("Z poprawką LaPlace'a: ", np.mean(with_laplace))
+
