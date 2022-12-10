bbd403546007dbb2e1bfbcb06515c6ac77e35e31
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-12-10 19:07:31 +0100

    mean and std computation

--- SI/NBC_C/NBC.py
+++ SI/NBC_C/NBC.py
@@ -9,36 +9,19 @@ class NBC(BaseEstimator, ClassifierMixin):
         pass
 
     def fit(self, X, y, LaPlace = False):
-        self.P_Y = self.Single_Prob(y, LaPlace)
+        self._mean =  self.mean(X[:,0], y)
+        self._std = self.std(X[:,0], y)
+        # self._density = self.density(X[:,0], y) ##TODO
 
 
     def accuracy_score(self, X_predict, y_test):
         res_list = X_predict == y_test
         return np.round((len(res_list[res_list==True])/len(res_list)) * 100)
 
-    def predict(self, P_X):
+    def predict(self, X_test):
         res = []
-        cnt = 0
-        temp = np.array(self.P_Y)[:,0]
-        for el in P_X:
-            P = []
-            for idx, classi_num in enumerate(temp):
-                prob = 1
-                for col in self.P_X_Y:
-                    for classi in col:
-                        if classi[0][0] == classi_num:
-                            for est in classi:
-                                if est[1] == el[cnt]:
-                                    prob *= est[2]
-                                    cnt += 1 
-                                    break
-                            break
-                cnt = 0       
-                prob *= self.P_Y[idx][2]
-                P.append(prob)
-            res.append(self.P_Y[P.index(max(P))][0])
-        return res
-    
+        return X_test [:,0]   
+
     def predict_proba(self, P_X):
         res = []
         cnt = 0
@@ -80,15 +63,37 @@ class NBC(BaseEstimator, ClassifierMixin):
                 P.append([el, dict(P_cnt)[el], dict(P_cnt)[el]/sum(P_cnt.values())]) 
         return P
 
-    def density(self, X, y):
-        pass
+    def density(self,X, y): ##TODO
+        p = []
+        for idx, el in enumerate(X):
+            p.append([y[idx], (1/(self._std*np.sqrt(2*np.pi))) * np.exp(-((el-self._mean)**2)/(2*(self._std**2)))])
+        return p
+        
 
     def std(self, X, y):
-        pass
+        m = len(X)
+        res = []
+        for classi in set(y):
+            sum = 0
+            for idx, el in enumerate(X):
+                if y[idx] == classi:
+                    sum += (el - self.mean(X,y)[int(classi-1)][1])**2 ##mean to TODO
+            sum = np.sqrt(sum * 1/(m-1))
+            res.append([classi, sum])
+        return res
 
     def mean(self, X, y):
-        pass
-    
+        m = len(X)
+        res = []
+        for classi in set(y):
+            sum = 0
+            for idx, el in enumerate(X):
+                if y[idx] == classi:
+                    sum += el
+            sum = sum * (1/m)
+            res.append([classi, sum])
+        return res
+
     # def Condi_Prob(self, X, y, LaPlace):
     #     '''Returns probability (first dimension is first column etc)'''
     #     P_semifinal = []
--- SI/NBC_C/testing.py
+++ SI/NBC_C/testing.py
@@ -18,21 +18,22 @@ without_laplace = []
 for i in range(1):
 
     X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.33)
-    nbc = NBC()
-    nbc.fit(X_train,y_train, LaPlace=False)
-    X_predict = nbc.predict(X_test)
-    prediction_prob = nbc.predict_proba(X_test)
-    with_laplace.append(nbc.accuracy_score(X_predict, y_test))
+    # nbc = NBC()
+    # nbc.fit(X_train,y_train, LaPlace=False)
+    # X_predict = nbc.predict(X_test)
+    # prediction_prob = nbc.predict_proba(X_test)
+    # with_laplace.append(nbc.accuracy_score(X_predict, y_test))
 
     nbc = NBC()
     nbc.fit(X_train,y_train, LaPlace=True)
     y_predict = nbc.predict(X_test)
-    prediction_prob = nbc.predict_proba(X_test)
-    nbc.accuracy_score(y_predict, y_test)
-    without_laplace.append(nbc.accuracy_score(y_predict, y_test))
+    print(y_predict)
+    # prediction_prob = nbc.predict_proba(X_test)
+    # nbc.accuracy_score(y_predict, y_test)
+    # without_laplace.append(nbc.accuracy_score(y_predict, y_test))
     
-print("Bez poprawki LaPlace'a: ", np.mean(without_laplace))
-print("Z poprawką LaPlace'a: ", np.mean(with_laplace))
+# print("Bez poprawki LaPlace'a: ", np.mean(without_laplace))
+# print("Z poprawką LaPlace'a: ", np.mean(with_laplace))
 
 # X = np.array(pd.read_csv("balance-scale.data"))
 # y = X[:,0]
