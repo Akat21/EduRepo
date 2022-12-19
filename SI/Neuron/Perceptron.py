0e8eab305bafda1fc51f02a1fe020dd197743a52
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-12-19 23:35:20 +0100

    working perceptron

--- SI/Neuron/Perceptron.py
+++ SI/Neuron/Perceptron.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
 import random 
+import collections
 
 def fs(x):
     return np.where(x > 0 , 1, -1)
@@ -19,19 +20,26 @@ class Perceptron:
         self.weights = np.zeros(n_features)
         self.k = 0
 
-        y_fs = fs(y)
+        y_fs = np.array(fs(y))
 
         while(True):
-            E = fs(np.dot(X, self.weights))
-            if len(list(set(y_fs == E))) == 1:
-                if list(set(y_fs == E))[0] == True:
-                    break
-
             r_idx = random.randint(0, len(X)-1)
 
-            update = self.lr * (y_fs[r_idx] - E[r_idx])
-            self.weights = self.weights + (update * X[r_idx])
-            self.k += 1
+            E = np.array(fs(np.dot(X, self.weights)))
+            
+            if (len(list(set(y_fs == E))) == 1) and (list(set(y_fs == E))[0] == True):
+                break
+
+            if E[r_idx] == y_fs[r_idx]:
+                continue
+            else:
+                update = self.lr * y_fs[r_idx]
+                self.weights = self.weights + (update * X[r_idx])
+                self.k += 1
 
     def predict(self, X):
-        return fs(np.dot(X,self.weights))
+        return fs(np.dot(X,self.weights)), self.k, self.weights
+
+    def accuracy_score(self, X_predict, y_test):
+        res_list = X_predict == y_test
+        return np.round((len(res_list[res_list==True])/len(res_list)) * 100)
