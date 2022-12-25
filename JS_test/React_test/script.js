d8ada5be5c263ea38adf1b1538a969f7f44de6af
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-12-25 18:47:25 +0100

    some practice

--- JS_test/React_test/script.js
+++ JS_test/React_test/script.js
@@ -1,1 +1,49 @@
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+//LEARN TO IMPORT REACT
+function Header(){
+    return (
+    <header>
+        <nav className="nav">
+            <img src="react_logo.png" className="nav-img"/>
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+
+function Footer(){
+    return (
+    <footer>
+        <small>© 2022 Sawiński development. All rigths reserved</small>
+    </footer>
+    )
+}
+
+function MainContent(){
+    return(
+    <div>
+        <h1>Why you should learn React</h1>
+        <ol>
+            <li>React is fun</li>
+            <li>React is declarative</li>
+            <li>React is composable</li>
+            <li>React is highly paid XD</li>
+        </ol>
+    </div>
+    )
+}
+
+function InfoPage(){ 
+    return (
+    <div>
+        <Header/>
+        <MainContent />
+        <Footer />
+    </div>
+    )
+}
+
+ReactDOM.render(<InfoPage />, document.getElementById('root'));
