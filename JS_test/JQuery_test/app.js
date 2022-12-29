ce38a5953c5571e55451ab964b9978a155ffa5c2
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-12-29 21:44:28 +0100

    Jquery beginning

---
+++ JS_test/JQuery_test/app.js
@@ -0,0 +1,10 @@
+// var script = document.createElement('script');
+// script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+// document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(()=>{
+    // $('#main').text('Working').css('color', 'red'); //css changes style in jquery
+    console.log('ready');
+});
+
+console.log("essa");
