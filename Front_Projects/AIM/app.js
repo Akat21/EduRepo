1c623df2450ad159227d358a15809a32ad417820
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2022-12-31 23:02:34 +0100

    login page:

--- Front_Projects/AIM/app.js
+++ Front_Projects/AIM/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 
 app.use('/', express.static(path.join(__dirname, 'static')));
+
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'static/index.html'));
 });
+
+app.set('view engine', 'ejs');
+app.get('/login', (req, res)=>{
+    res.render('login_base', {title:"Login system"});
+});
+
+// app.post('/login', (req, res)=>{
+//     console.log(req);
+// });
+
 
 app.listen(3000);
