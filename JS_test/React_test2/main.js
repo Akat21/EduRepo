be8342894672bcfc258c5d979f1127d2b586a246
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-01-05 19:35:59 +0100

    setup of webpack

---
+++ JS_test/React_test2/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const x = 0;
+function Hi(){
+    return (
+        <h1>Hello</h1>
+    );
+}
+ReactDOM.render(<Hi />, document.getElementById('root'));
