ccd01db1614e047d0dc624caf592e99b4b5c93eb
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-01-16 22:45:05 +0100

    IDK

--- SI/MLP/MLP.py
+++ SI/MLP/MLP.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class MLP(object):
-    def __init__(self, hidden = 10, epochs = 100, eta = 0.1, shuffle = True):
+    def __init__(self, hidden = 5, epochs = 100, eta = 0.1, shuffle = True):
         self.hidden = hidden
         self.epochs = epochs
         self.eta = eta
@@ -12,9 +12,9 @@ class MLP(object):
 
     def _forward(self, X):
         classes = 3
-        self.w_h = np.array(len(X) * [np.zeros(len(X) + 1) + 0.01])
-        self.w_o = np.array((len(X) + 1) * [np.zeros(classes) + 0.01])
-        self.b_h = np.array(np.ones(len(X) + 1))
+        self.w_h = np.array(len(X) * [np.zeros(self.hidden) + 0.01])
+        self.w_o = np.array(self.hidden * [np.zeros(classes) + 0.01])
+        self.b_h = np.array(np.ones(self.hidden))
         self.b_o = np.array(np.ones(classes))
         out_h = self._sigmoid(np.dot(X, self.w_h) + self.b_h)
         out_o = self._sigmoid(np.dot(out_h, self.w_o) + self.b_o)
@@ -28,13 +28,13 @@ class MLP(object):
         return -final_res
 
     def fit(self, X_train, y_train):
-        X_train = np.column_stack((X_train.index, np.array(X_train)))
+        X_train = np.array(X_train)
         y_train = np.column_stack(((np.array(y_train)[:,0] - 1), np.array(y_train)[:,1]))
-        w_h = np.array(len(X_train[0]) * [np.zeros(len(X_train[0]) + 1) + 0.01])
-        w_o = np.array((len(X_train[0]) + 1) * [np.zeros(len(y_train[0][1])) + 0.01])
-        b_h = np.zeros(len(X_train[0] + 1))
+        w_h = np.random.randn(len(X_train[0]), self.hidden)
+        w_o = np.random.randn(self.hidden, len(y_train[0][1]))
+        b_h = np.zeros(self.hidden)
         b_o = np.zeros(len(y_train[0][1]))
-        
+
         #SHUFFLE 
         if self.shuffle == True:
             idxs = np.arange(X_train.shape[0])
@@ -49,4 +49,10 @@ class MLP(object):
             delta_o = (a_o - y_train[idx,1]) * f_o
             f_h = (a_h * (1 - a_h))
             delta_h = np.dot(delta_o, np.transpose(w_o)) * f_h
-            print(delta_h)
+            _X_train = np.insert(X_train[idx], 0, 1)
+            grad_w_h = np.dot(np.transpose(_X_train), delta_h)
+            grad_b_h = delta_h
+            grad_w_o = np.dot(np.transpose(a_h), delta_o)
+            print(grad_w_o)
+            break
+            ##GRADINET TODO
