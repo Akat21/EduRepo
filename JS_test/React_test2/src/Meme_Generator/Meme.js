76f216a15ff13610477500bd9ad25ddc56e46f78
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-01-21 19:26:02 +0100

    practice

--- JS_test/React_test2/src/Meme_Generator/Meme.js
+++ JS_test/React_test2/src/Meme_Generator/Meme.js
@@ -1,13 +1,24 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme(){
+    const [memeImg, setMemeImg] = React.useState("");
+
+    function getMemeImg(){
+        const memesArray = memesData.data.memes;
+        const randomNum = Math.floor(Math.random() * (memesArray.length));
+        console.log(randomNum)
+        setMemeImg(memesArray[randomNum].url)
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input className="form--input" type="text"></input>
                 <input className="form--input" type="text"></input>
-                <button className="form--button">Get a new meme image</button>
-            </form>
+                <button className="form--button" onClick={getMemeImg}>Get a new meme image</button>
+            </div>
+            <img src={memeImg} className="meme--img"/>
         </main>
     )
 }
--- JS_test/React_test2/src/memesData.js
+++ JS_test/React_test2/src/memesData.js
@@ -1,13 +1,21 @@
 export default{
     "success": true,
     "data": {
-        "memes": {
+        "memes": [{
             "id": 128390,
             "name": "Drake Hotline Bling",
-            "url": "https://i.imgflip.com/30b1gx.jpg",
+            "url": "./src/assets/drake.jpg",
             "width": 1200,
             "height": 1200,
             "box_count": 2
-        }
+        },
+        {
+            "id": 121320,
+            "name": "Princess",
+            "url": "./src/assets/princess.jpg",
+            "width": 1200,
+            "height": 1200,
+            "box_count": 2
+        }]
     }
 }
