e32a659686389ce7c77a67451a9cd98f3cc862ba
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-01-26 15:25:55 +0100

    translate speech in polish into english

--- KCM/syntezator/Syntezator.py
+++ KCM/syntezator/Syntezator.py
@@ -1,37 +1,32 @@
-import pyaudio
 from googletrans import Translator
 import speech_recognition as sr
 import playsound
 from gtts import gTTS
-import os
 
-def recognize():
+def recognize(filename = None, lang = "pl_PL"):
     r = sr.Recognizer()
     r.energy_threshold = 800
     with sr.Microphone() as source:
         audio = r.listen(source)
-        said = r.recognize_google(audio, language="pl_PL")
-    save(said)
+        said = r.recognize_google(audio, language=lang)
     return said
 
-def speak(txt):
-    filename = save(txt)
+def speak(txt, filename):
     playsound.playsound(filename)
 
-def save(txt):
+def save(txt, filename):
     tts = gTTS(text=txt, lang='pl')
-    filename = "voicePl.mp3"
     tts.save(filename)
     return filename
 
 def translate(txt):
     translator = Translator()
-    txt = "Ładna dziś pogoda"
     translated = translator.translate(txt)
+    save(translated.text, "voiceEn.mp3")
     return translated.text
 
-text = recognize()
-# text = translate("dsda")
-print(text)
-speak(text)
+text_pl = recognize("voicePl.mp3")
+save(text_pl, "voicePl.mp3")
+text_en = translate(text_pl)
+speak(text_en, "voiceEn.mp3")
 
