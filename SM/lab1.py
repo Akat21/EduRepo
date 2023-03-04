482cbe43067d8b9771e70a60b1e5f65c9586dcdb
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-04 11:49:38 +0100

    done i guess

--- SM/lab1.py
+++ SM/lab1.py
@@ -38,65 +38,68 @@ def convertToGrayY1(img):
     B = img[:,:,2] * 0.114
     Y1 = R + G + B
     return Y1
- 
-#oryginalny
-plt.subplot(3,3,1)
-plt.imshow(img1)
 
-#Y1
-plt.subplot(3,3,2)
-plt.imshow(convertToGrayY1(img1), cmap=plt.cm.gray)
+def DrawOut(img): 
+    #oryginalny
+    plt.subplot(3,3,1)
+    plt.imshow(img)
 
-#Y2
-plt.subplot(3,3,3)
-plt.imshow(convertToGrayY2(img1), cmap=plt.cm.gray)
+    #Y1
+    plt.subplot(3,3,2)
+    plt.imshow(convertToGrayY1(img), cmap=plt.cm.gray)
 
-#R
-R = img1[:,:,0] * 0.299
-plt.subplot(3,3,4)
-plt.imshow(R, cmap=plt.cm.gray)
+    #Y2
+    plt.subplot(3,3,3)
+    plt.imshow(convertToGrayY2(img), cmap=plt.cm.gray)
 
-#G
-G = img1[:,:,1] * 0.587
-plt.subplot(3,3,5)
-plt.imshow(G, cmap=plt.cm.gray)
+    #R
+    R = img[:,:,0] * 0.299
+    plt.subplot(3,3,4)
+    plt.imshow(R, cmap=plt.cm.gray)
 
-#B
-B = img1[:,:,2] * 0.114
-plt.subplot(3,3,6)
-plt.imshow(R, cmap=plt.cm.gray)
+    #G
+    G = img[:,:,1] * 0.587
+    plt.subplot(3,3,5)
+    plt.imshow(G, cmap=plt.cm.gray)
 
-#R0
-img_R0 = img1.copy()
-for i in range(3):
-    if i != 0:
-        img_R0[:,:,i] = 0
-plt.subplot(3,3,7)
-plt.imshow(img_R0, cmap=plt.cm.gray)
+    #B
+    B = img[:,:,2] * 0.114
+    plt.subplot(3,3,6)
+    plt.imshow(R, cmap=plt.cm.gray)
 
-#G0
-img_G0 = img1.copy()
-for i in range(3):
-    if i != 1:
-        img_G0[:,:,i] = 0
-plt.subplot(3,3,8)
-plt.imshow(img_G0, cmap=plt.cm.gray)
+    #R0
+    img_R0 = img.copy()
+    for i in range(3):
+        if i != 0:
+            img_R0[:,:,i] = 0
+    plt.subplot(3,3,7)
+    plt.imshow(img_R0, cmap=plt.cm.gray)
 
-#B0
-img_B0 = img1.copy()
-for i in range(3):
-    if i != 2:
-        img_B0[:,:,i] = 0
+    #G0
+    img_G0 = img.copy()
+    for i in range(3):
+        if i != 1:
+            img_G0[:,:,i] = 0
+    plt.subplot(3,3,8)
+    plt.imshow(img_G0, cmap=plt.cm.gray)
 
-plt.subplot(3,3,9)
-plt.imshow(img_B0, cmap=plt.cm.gray)
-plt.show()
+    #B0
+    img_B0 = img.copy()
+    for i in range(3):
+        if i != 2:
+            img_B0[:,:,i] = 0
+
+    plt.subplot(3,3,9)
+    plt.imshow(img_B0, cmap=plt.cm.gray)
+parametr = "Normal"
+DrawOut(img1)
+plt.savefig("Colors-{}".format(parametr))
 ######
 
 ##ZAD3
 parametr = "Normal"
 img = plt.imread('B02.jpg')
 fragment = img[500:700, 600:800].copy()
-plt.imshow(fragment)
+DrawOut(fragment)
 plt.savefig("Tęcza-{}".format(parametr))
 ######
