ddfeb3998cb654badfab1728dfde94dbac9a3c9b
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-05 12:18:32 +0100

    lab1 done

--- sym/lab0.py
+++ sym/lab0.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import cm
 import matplotlib.pyplot as plt
 
 x = np.linspace(0.1,4)
@@ -20,23 +21,27 @@ plt.show()
 x = np.linspace(-2*np.pi, 2*np.pi)
 y = np.linspace(-2*np.pi, 2*np.pi)
 
+#aby wyznaczyć Z musimy użyć meshgrida na naszych wartościach x,y 
 xx, yy = np.meshgrid(x,y)
-zz = np.sqrt(xx**2 + yy**2)
+zz = f3(xx,yy)
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.contour3D(xx,yy,zz,100,cmap='binary')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+#rysowanie 3D plota
+fig, ax = plt.subplots(subplot_kw={"projection" : "3d"})
 
+ax.plot_surface(xx, yy, zz, cmap='viridis')
+#####
+
+#uniform wypisuje random floaty z przedzialu o wielkosci size
 ran_x = np.random.uniform(2,5, size = 7)
 ran_y = np.random.uniform(3,7, size = 7)
 ran_points = np.column_stack((ran_x, ran_y))
 
 def draw_points(x, y):
+    #scatter rysuje punkty
     for i in range(len(x)):
         ax.scatter(x[i], y[i], marker='s', c='red', s=15)
+    for i in range(len(x) - 1):
+        ax.plot([x[i], x[i+1]], [y[i], y[i+1]], c='black', linestyle='--', linewidth=3)
 
 draw_points(ran_x, ran_y)
 plt.show()
