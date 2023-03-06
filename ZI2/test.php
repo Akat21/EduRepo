f972859f0bdfd76534065555bc3af10992f098a0
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-06 11:07:46 +0100

    testing

--- ZI2/test.php
+++ ZI2/test.php
@@ -3,5 +3,6 @@
     define("id", 2401);
     echo($name."\n".id);
     $surnames = array("Sawicky", "Sawinsky");
-    echo("\n$surnames[1]")
+    // echo("\n$surnames[1]");
+    echo("\n".$_SERVER['SCRIPT_NAME']);
 ?>
