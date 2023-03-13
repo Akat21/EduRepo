a2c56a6b1d3436d398352fc9be2815ebb8db5aa7
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-13 13:44:52 +0100

    lab3

--- ZI2/lab03/src/main/java/org/example/Main.java
+++ ZI2/lab03/src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import java.sql.PreparedStatement;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 
@@ -10,10 +13,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         
-        
+        Connection conn; 
+
         try{
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/zi2", "root", "KakaPL1234");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/zi2", "root", "KakaPL1234");
+            System.out.println("Połączono!");
             Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS tabela (id INT NOT NULL AUTO_INCREMENT, napis TEXT, liczba INT, PRIMARY KEY(id))");
+            stmt.execute("DELETE FROM tabela");
+            stmt.execute("INSERT INTO tabela(napis, liczba) VALUES('someText', 12)");
+            stmt.execute("INSERT INTO tabela(napis, liczba) VALUES('someText2', 125)");
+            
+            String[] arr = new String[] {"jeden", "dwa", "trzy", "cztery", "piec", "szesc", "siedem", "osiem", "dziewiec", "dzisiec"};
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO tabela(napis,liczba) VALUES(?,?)");
+            for(String el : arr){
+                ps.setString(1, el);
+                ps.setInt(2, (int)Math.floor(Math.random() * (200 - 0 + 1) + 0));
+                ps.executeUpdate();
+            }
+
             ResultSet rs = stmt.executeQuery("SELECT * FROM tabela");
             while(rs.next()){
                 System.out.println(rs.getString("napis") + " " + rs.getInt("liczba"));
