7b41c3acd04d50b73d78ed8e8dcc841e78c31065
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-24 16:37:49 +0100

    changes

--- SM/lab4/main.py
+++ SM/lab4/main.py
@@ -40,6 +40,7 @@ pallet16 =  np.array([
         [1.0, 1.0, 0.0,]
 ])
 
+##KWANTYZACJA##
 def colorFit(pixel, pallet):
     if(len(img.shape) < 3):
         pts = np.abs(pallet - pixel)
@@ -50,14 +51,39 @@ def colorFit(pixel, pallet):
         return pallet[np.argmin(pts)]
 
 def kwant_colorFit(img, pallet):
-    out_img = img.copy()
-    height, width, dim = img.shape
+    if(len(img.shape) < 3):
+        out_img = img.copy()
+        height, width = img.shape
 
-    for i in range(height):
-        for j in range(width):
-            out_img[i, j] = colorFit(img[i, j], pallet)
+        for i in range(height):
+            for j in range(width):
+                out_img[i, j] = colorFit(img[i, j], pallet)
+    else:
+        out_img = img.copy()
+        height, width, dim = img.shape
+
+        for i in range(height):
+            for j in range(width):
+                out_img[i, j] = colorFit(img[i, j], pallet)
     return out_img
 
-q_img = kwant_colorFit(img, pallet8)
-plt.imshow(q_img)
+###
+# q_img = kwant_colorFit(img, pallet8)
+def dithering(img):
+    if(len(img.shape) < 3):
+        height, width = img.shape
+        r = np.random.rand(height, width)
+        binary_img = (img >= r) * 1
+        return binary_img
+    else:
+        gray_img = np.dot(img[...,:3], [0.2989, 0.5870, 0.1140])
+        
+        height, width = gray_img.shape
+        r = np.random.rand(height, width)
+        binary_img = (gray_img >= r) * 1
+        return binary_img
+
+new_img = dithering(img)
+print(new_img)
+plt.imshow(new_img)
 plt.show()
--- mobilne/lab2.py
+++ mobilne/lab2.py
@@ -10,13 +10,13 @@ def ITU_R(f, d, N, Lf):
     return 20 * np.log10(f) + N * np.log10(d) + Lf - 28
 
 def OneSlope(d, L0, lmbda):
-    return swobodna(f, d) + 10*lmbda*np.log(d)
+    return swobodna(f, d) + 10*lmbda*np.log10(d)
 
 def MotleyKeenan(f, d, nw, Lw, nf, Lf):
     return swobodna(f, d) + nw * Lw + nf * Lf
 
 def MultiWall(d, L0, lmbda, kw, Lw, kf, Lf):
-    return L0 + 10 * lmbda * np.log(d) + np.sum(kw*Lw) + np.sum(kf*Lf)
+    return L0 + 10 * lmbda * np.log10(d) + np.sum(kw*Lw) + np.sum(kf*Lf)
 
 def EnBilans(Pn, Gn, Go, L, A):
     return Pn + Gn + Go - L - A
@@ -26,9 +26,8 @@ d = 80
 f = 5220
 nw = 2
 ns = 4
-print("Tłumienie w postaci swobodnej: ", swobodna(f, d))
-print("ITU-R P.1238: ", ITU_R(f, d, 31, 1))
-print("OneSlope: ", OneSlope(d, swobodna(f, d), 3))
-print("Motley Keendan: ", MotleyKeenan(f, d, nw, 9, ns, 20))
-print("Model Multi-Wall: ", MultiWall(d, swobodna(f,d ), 3, nw, 9, ns, 11))
-print("Bilans Energetyczny: ", EnBilans(50, 50, 50, swobodna(f, d), 50))
+print("Tłumienie w postaci swobodnej: ", EnBilans(20, 20, 20, swobodna(f, d), 20))
+print("ITU-R P.1238: ", EnBilans(20, 20, 20, ITU_R(f, d, 31, 1), 20))
+print("OneSlope: ", EnBilans(20, 20, 20, OneSlope(d, swobodna(f, d), 2), 20))
+print("Motley Keendan: ", EnBilans(20, 20, 20, MotleyKeenan(f, d, nw, 9, ns, 20), 20))
+print("Model Multi-Wall: ", EnBilans(20, 20, 20, MultiWall(d, swobodna(f, d), 2, nw, 9, ns, 11), 20))
