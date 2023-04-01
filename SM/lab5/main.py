89d14cbe0795851c7cbfec474f975303a493e795
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-01 13:08:22 +0200

    lab5 implementation of algorithms

--- SM/lab5/main.py
+++ SM/lab5/main.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 import soundfile as sf
+from scipy.interpolate import interp1d
 
 data, fs = sf.read('./lab5/sin_60Hz.wav', dtype='int32')
 
 sin_data = np.sin(np.linspace(0, 10)) 
 plt.plot(sin_data)
 plt.show()
+
 def Kwant(data, bit):
     d = bit
     if np.issubdtype(data.dtype, np.floating):
@@ -20,9 +22,27 @@ def Kwant(data, bit):
     dataF = (np.round((data - m) / (n - m) * d) / d * (n-m) + m).astype(int)
     return dataF.astype(data.dtype)
 
+def Decymacja(data, n):
+    return data[::n], fs/n
 
+def InterpLine(data, new_fs):
+    x = np.linspace(0, len(data), len(data))
+    y = data
+    interp = interp1d(x, y)
+    y_lin = interp(np.linspace(0, len(data), new_fs))
+    return y_lin
 
-sin_data = Kwant(sin_data, 4)
-plt.plot(sin_data)
+def InterpNonLine(data, new_fs):
+    x = np.linspace(0, len(data), len(data))
+    y = data
+    interp = interp1d(x, y, kind='cubic')
+    y_lin = interp(np.linspace(0, len(data), new_fs))
+    return y_lin
+
+# data = InterpLine(data, 2400)
+# data = InterpNonLine(data, 2400)
+# data, fs = Decymacja(data, 10)
+# data = Kwant(data, 32)
+plt.plot(data)
 plt.show()
 
