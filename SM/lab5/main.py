635995f4a794c4145bd00fe05d9fed52b02a1814
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-02 15:16:42 +0200

    lab5

--- SM/lab5/main.py
+++ SM/lab5/main.py
@@ -1,17 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
+import scipy.fftpack
 import soundfile as sf
 from scipy.interpolate import interp1d
 
-data, fs = sf.read('./lab5/sin_60Hz.wav', dtype='int32')
+data, fs = sf.read('./lab5/sin_combined.wav', dtype=np.int32)
+print(fs)
+# sin_data = np.sin(np.linspace(0, 10)) 
+# plt.plot(sin_data)
+# plt.show()
 
-sin_data = np.sin(np.linspace(0, 10)) 
-plt.plot(sin_data)
-plt.show()
+def plotAudio(Signal, fs, TimeMargin=[0,0.15]):
+    fsize = 256
+    plt.subplot(2,1,1)
+    plt.plot(np.arange(0,Signal.shape[0])/fs, Signal)
+    plt.xlim(TimeMargin)
+
+    plt.subplot(2,1,2)
+    yf = scipy.fftpack.fft(Signal,fsize)
+    plt.plot(np.arange(0,fs/2,fs/fsize), 20*np.log10(np.abs(yf[:fsize//2])))
+    plt.show()
 
 def Kwant(data, bit):
-    d = bit
+    d = 2**bit - 1
     if np.issubdtype(data.dtype, np.floating):
         m = 0
         n = 1
@@ -19,10 +31,10 @@ def Kwant(data, bit):
         m = data.min()
         n = data.max()
     dataF = data.astype(float)
-    dataF = (np.round((data - m) / (n - m) * d) / d * (n-m) + m).astype(int)
+    dataF = (np.round((dataF - m) / (n - m) * d) / d * (n-m) + m).astype(int)
     return dataF.astype(data.dtype)
 
-def Decymacja(data, n):
+def Decymacja(data, n, fs):
     return data[::n], fs/n
 
 def InterpLine(data, new_fs):
@@ -30,19 +42,20 @@ def InterpLine(data, new_fs):
     y = data
     interp = interp1d(x, y)
     y_lin = interp(np.linspace(0, len(data), new_fs))
-    return y_lin
+    return y_lin, new_fs
 
 def InterpNonLine(data, new_fs):
     x = np.linspace(0, len(data), len(data))
     y = data
     interp = interp1d(x, y, kind='cubic')
     y_lin = interp(np.linspace(0, len(data), new_fs))
-    return y_lin
+    return y_lin, new_fs
 
-# data = InterpLine(data, 2400)
-# data = InterpNonLine(data, 2400)
-# data, fs = Decymacja(data, 10)
-# data = Kwant(data, 32)
-plt.plot(data)
-plt.show()
+data, fs = InterpLine(data, 41000)
+# data, fs = InterpNonLine(data, 16950)
+# data, fs = Decymacja(data, 1, fs)
+# data = Kwant(data, 24)
+plotAudio(data, fs, [0.005, 0.1])
+# plt.plot(data)
+# plt.show()
 
