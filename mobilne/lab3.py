90d0fafb4e53db12f39cd447644fd75226236db2
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-13 19:04:41 +0200

    mobilne

--- mobilne/lab3.py
+++ mobilne/lab3.py
@@ -12,10 +12,10 @@ B = wavelength/2
 H = wavelength
 D = wavelength/8
 
-L2 = L1*8
+L2 = L1*8 + D
 max_length = 8 * L1 + D + 2*H + 2*B
 
-print("L1: ", L1, "L2: ", L2, "B: ", B, "H: ", H, "D: ", "Całkowita długość drutu: ", max_length)
+print("L1: ", L1, "L2: ", L2, "B: ", B, "H: ", H, "D: ", D ,"Całkowita długość drutu: ", max_length)
 
 #802.11g
 print("BiQuad\n802.11g")
@@ -28,10 +28,10 @@ B = wavelength/2
 H = wavelength
 D = wavelength/8
 
-L2 = L1*8
+L2 = L1*8 + D
 max_length = 8 * L1 + D + 2 * H + 2 * B
 
-print("L1: ", L1, "L2: ", L2, "B: ", B, "H: ", H, "D: ", "Całkowita długość drutu: ", max_length)
+print("L1: ", L1, "L2: ", L2, "B: ", B, "H: ", H, "D: ", D, "Całkowita długość drutu: ", max_length)
 
 #Yagi-Uda
 #802.11g
@@ -46,7 +46,7 @@ direktor_l = 0.44 * wavelength
 reflektor_radiator = 0.25 * wavelength
 radiator_direktor = 0.20 * wavelength
 
-max_length = reflektor_l + reflektor_radiator + radiators_n * (radiator_l + radiator_direktor) + direktor_l
+max_length = reflektor_l + reflektor_radiator + radiators_n * (radiator_l + radiator_direktor) + radiator_direktor + direktor_l
 
 print("Całkowita długość drutu: ", max_length)
 
@@ -62,6 +62,25 @@ direktor_l = 0.44 * wavelength
 reflektor_radiator = 0.25 * wavelength
 radiator_direktor = 0.20 * wavelength
 
-max_length = reflektor_l + reflektor_radiator + radiators_n * (radiator_l + radiator_direktor) + direktor_l
+max_length = reflektor_l + reflektor_radiator + radiators_n * (radiator_l + radiator_direktor) + radiator_direktor + direktor_l
 
 print("Całkowita długość drutu: ", max_length)
+
+#LOG-PER
+#802.11g
+print("LOG-PER\n802.11g")
+f = 2.4 * 1000000000
+c = 300000000
+wavelength = c / f
+T = 0.7
+dipols = 8
+
+I0 = wavelength/2
+I1 = T*I0
+
+l = [I0]
+for i in range(1, dipols, 1):
+    l.append(0)
+    l[i] = l[i-1]*T
+
+print("Długości poszczególnych dipoli(dla 8):", l, "Całkowita Długość: ", sum(l))
