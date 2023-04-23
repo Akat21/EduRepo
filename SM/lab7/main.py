e115b49493a73255b626491ca5f99e1a9819ea6f
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-23 13:21:04 +0200

    DPCM

--- SM/lab7/main.py
+++ SM/lab7/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import soundfile as sf
 import sounddevice as sd
 
-sin_data, fs = sf.read('./lab7/sing_medium2.wav', dtype=np.float32)
+sin_data, fs = sf.read('./lab7/sing_high2.wav', dtype=np.float32)
 sd.play(sin_data, fs)
 status = sd.wait()
 # x=np.linspace(-1,1,1000)
@@ -23,7 +23,7 @@ def Kwant(data, bit):
     dataF = (np.round((dataF - m) / (n - m) * d) / d * (n-m) + m)
     return dataF.astype(data.dtype)
 
-def ALawEncoding(data):
+def ALawEncoding(data, bit):
     A = 87.6
     
     Fx = data.copy()
@@ -31,7 +31,7 @@ def ALawEncoding(data):
     idx2 = np.logical_and((1/A) <= np.abs(Fx), np.abs(Fx) <= 1)
     Fx[idx] = np.sign(data[idx]) * (A*np.abs(data[idx]))/(1 + np.log(A))
     Fx[idx2] = np.sign(data[idx2]) * (1 + np.log(A*np.abs(data[idx2])))/(1+np.log(A))
-    Fx = Kwant(Fx, 8)
+    Fx = Kwant(Fx, bit)
     return Fx
 
 def ALawDecoding(data):
@@ -43,12 +43,12 @@ def ALawDecoding(data):
     Fy[idx2] = np.sign(data[idx2]) * (np.exp(np.abs(data[idx2])*(1+np.log(A)-1)))/A
     return Fy
 
-def MuLawEncoding(data):
+def MuLawEncoding(data, bit):
     mu = 255
     Fx = data.copy()
     idx = np.logical_and(-1 <= data, data <= 1)
     Fx[idx] = np.sign(data[idx]) * ((np.log(1 + mu * np.abs(data[idx])))/np.log(1+mu))
-    Fx = Kwant(Fx, 8)
+    Fx = Kwant(Fx, bit)
     return Fx
 
 def MuLawDecoding(data):
@@ -58,8 +58,25 @@ def MuLawDecoding(data):
     Fy[idx] = np.sign(data[idx]) * (1 / mu) * ( (1 + mu)**np.abs(data[idx]) - 1)
     return Fy
 
-sin_data = ALawEncoding(sin_data)
-sin_data = ALawDecoding(sin_data)
+
+def DPCMEncoding(x, bit):
+    y = np.zeros(x.shape)
+    e = 0
+    for i in range(0, x.shape[0]):
+        y[i] = Kwant(x[i] - e, bit)
+        e += y[i]
+    return y
+
+def DPCMDecoding(y):
+    x = np.zeros(y.shape)
+    e = 0
+    for i in range(0,x.shape[0]):
+        x[i] = y[i] + e
+        e = x[i]
+    return x
+
+sin_data = DPCMEncoding(sin_data, 8)
+sin_data = DPCMDecoding(sin_data)
 plt.plot(sin_data)
 plt.show()
 sd.play(sin_data, fs)
